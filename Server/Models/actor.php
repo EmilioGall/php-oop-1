d7ad352e872aac665dd6ee7fc16df374afc756cf
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-02 18:58:30 +0200

    feat: general setter&getter for the class - actor.php

--- Server/Models/actor.php
+++ Server/Models/actor.php
@@ -9,4 +9,55 @@ class Actor
    private string $gender;
    private string $role;
 
+   public function __construct(string $input_name, string $input_surname)
+   {
+
+      $this->name      = $input_name;
+      $this->surname   = $input_surname;
+   }
+
+   public function getName()
+   {
+
+      return $this->name;
+   }
+
+   public function getSurname()
+   {
+
+      return $this->surname;
+   }
+
+   public function setBirthday(array $input_date_birth)
+   {
+
+      $this->date_birth = $input_date_birth;
+   }
+   public function getBirthday()
+   {
+
+      return $this->date_birth;
+   }
+
+   public function setGender(array $input_gender)
+   {
+
+      $this->gender = $input_gender;
+   }
+   public function getGender()
+   {
+
+      return $this->gender;
+   }
+
+   public function setRole(array $input_role)
+   {
+
+      $this->role = $input_role;
+   }
+   public function getRole()
+   {
+
+      return $this->role;
+   }
 }
