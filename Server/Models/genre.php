525c78e568fd20588877d8bee9dc9587282b8846
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-02 12:08:40 +0200

    feat: added simple control on class - genre.php

--- Server/Models/genre.php
+++ Server/Models/genre.php
@@ -8,7 +8,40 @@ class Genre
    public function __construct(string $input_genre)
    {
 
-      $this->genre = $input_genre;
+      $genres_array = [
+         'Animation',
+         'Action',
+         'Adventure',
+         'Comedy',
+         'Crime',
+         'Drama',
+         'Fantasy',
+         'Horror',
+         'Musical',
+         'Thriller'
+      ];
+
+      $isFound = false;
+
+      foreach ($genres_array as $genre) {
+
+         if ($input_genre === $genre) {
+
+            $this->genre = $input_genre;
+
+            $isFound = true;
+         }
+
+         // var_dump($isFound);
+      }
+
+      if ($isFound === false) {
+
+         var_dump("Genre not allowed");
+      } else {
+
+         var_dump("Genre allowed");
+      }
    }
 
    public function getPrimaryGenre()
@@ -16,5 +49,4 @@ class Genre
 
       return $this->genre;
    }
-
 }
